1718784c989c438239a17b7fa85757c204052415
Scott Robertson <srobertson@codeit.com>
2013-07-24 10:44:49 -0700

    Version Bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 setup(
   name='splicer',
 
-  version='0.0.1',
+  version='0.0.3',
   description='the world is a database',
   long_description=README,
   author='Scott Robertson',
