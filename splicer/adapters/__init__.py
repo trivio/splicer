82cc2a0e61e4ab98c461e4fb5f76526167f6582c
Scott Robertson <srobertson@codeit.com>
2014-01-19 22:38:31 -0800

    raise the real NotImplementedError

--- splicer/adapters/__init__.py
+++ splicer/adapters/__init__.py
@@ -15,7 +15,6 @@ class Adapter(object):
     """
     return []
     
-    
   def get_relation(self, name):
     """Return the relation (table, view, etc..) with the given name or None if this
     adapter does not have the given table."""
@@ -24,7 +23,20 @@ class Adapter(object):
     """Return true if the Adapter can resolve the relation"""
 
   def table_scan(self, name, ctx):
-    raise NotImplemented()
+     raise NotImplementedError(
+      '{} has not implemented table_scan'.format(
+        self.__class__.__name__
+      )
+    )
+
 
   def evaluate(self, loc):
     return loc.replace(partial(self.table_scan, loc.node().name))
+
+  def schema(self, name):
+    raise NotImplementedError(
+      '{} has not implemented schema'.format(
+        self.__class__.__name__
+      )
+    )
+  
